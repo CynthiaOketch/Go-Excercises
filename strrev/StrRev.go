4ff41f7f13af1bafa9f0cd8dd0827836f61ca93e
coketch <cynthiaoketch6@gmail.com>
2024-07-10 14:28:44 +0300

    refactor: file organization

--- strrev/StrRev.go
+++ strrev/StrRev.go
@@ -2,15 +2,22 @@ package main
 
 import "fmt"
 
+//	func StrRev(s string) string {
+//		runes := []rune(s)
+//		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+//			runes[i], runes[j] = runes[j], runes[i]
+//		}
+//		return string(runes)
+//	}
 func StrRev(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+	var str2 string
+	for i := range s {
+		str2 += string(s[len(s)-i-1])
 	}
-	return string(runes)
+	return str2
 }
 
-/*func main() {
+func main() {
 	word := "Hello"
 	fmt.Println(StrRev(word))
 }
