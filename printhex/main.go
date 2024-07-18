bd3e7d78c038c3ebebb72685d64a9e7eec4fc84e
CynthiaoOketch.com <cynthiaoketch6@gmail.com>
2024-07-18 14:10:14 +0300

    update code

--- printhex/main.go
+++ printhex/main.go
@@ -1,20 +1,50 @@
-printhex
-Instructions
+// printhex
+// Instructions
 
-Write a program that takes a positive (or zero) number expressed in base 10, and that displays it in base 16 (with lowercase letters) followed by a newline ('\n').
+// Write a program that takes a positive (or zero) number expressed in base 10, and that displays it in base 16 (with lowercase letters) followed by a newline ('\n').
 
-    If the number of arguments is different from 1, the program displays nothing.
-    Error cases have to be handled as shown in the example below.
+//     If the number of arguments is different from 1, the program displays nothing.
+//     Error cases have to be handled as shown in the example below.
 
-Usage
+// Usage
 
-$ go run . 10
-a
-$ go run . 255
-ff
-$ go run . 5156454
-4eae66
-$ go run .
-$ go run . "123 132 1" | cat -e
-ERROR$
-$
+// $ go run . 10
+// a
+// $ go run . 255
+// ff
+// $ go run . 5156454
+// 4eae66
+// $ go run .
+// $ go run . "123 132 1" | cat -e
+// ERROR$
+// $
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) == 2 {
+		n, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Println("ERROR")
+			return
+		}
+		res := printHex(n, "0123456789abcdef")
+		fmt.Println(res)
+	}
+}
+
+func printHex(num int, base string) string {
+	result := ""
+
+	for num > 0 {
+		rem := num % len(base)
+		result = string(base[rem]) + result
+		num /= len(base)
+	}
+	return result
+}
--- sortword/main.go
+++ sortword/main.go
@@ -1,33 +1,53 @@
-sortwordarr
-Instructions
+// sortwordarr
+// Instructions
 
-Write a function SortWordArr that sorts by ascii (in ascending order) a string slice.
-Expected function
+// Write a function SortWordArr that sorts by ascii (in ascending order) a string slice.
+// Expected function
 
-func SortWordArr(a []string) {
+// func SortWordArr(a []string) {
 
-}
+// }
 
-Usage
+// Usage
 
-Here is a possible program to test your function :
+// Here is a possible program to test your function :
 
+// package main
+
+// import (
+// 	"fmt"
+// 	"piscine"
+// )
+
+// func main() {
+// 	result := []string{"a", "A", "1", "b", "B", "2", "c", "C", "3"}
+// 	piscine.SortWordArr(result)
+
+// 	fmt.Println(result)
+// }
+
+// And its output :
+
+// $ go run .
+// [1 2 3 A B C a b c]
+// $
 package main
 
-import (
-	"fmt"
-	"piscine"
-)
+import "fmt"
+
+func SortWordArr(a []string) {
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < len(a)-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
+}
 
 func main() {
 	result := []string{"a", "A", "1", "b", "B", "2", "c", "C", "3"}
-	piscine.SortWordArr(result)
+	SortWordArr(result)
 
 	fmt.Println(result)
 }
-
-And its output :
-
-$ go run .
-[1 2 3 A B C a b c]
-$
